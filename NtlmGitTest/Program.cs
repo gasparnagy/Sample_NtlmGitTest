19dcad75e638b949d72da2f794391d1ee6e1aa5f
Gaspar Nagy <gaspar.nagy@gmail.com>
2013-11-12 20:58:02 +0100

    repro for 401 error

--- NtlmGitTest/Program.cs
+++ NtlmGitTest/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using LibGit2Sharp;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,15 @@ namespace NtlmGitTest
     {
         static void Main(string[] args)
         {
+            const string cloneUrl = "https://tfs02.techtalk.at/tfs/Playground/_git/SpecLog.TestProject.Git";
+            var localPath = Environment.ExpandEnvironmentVariables(@"%TMP%\NtlmGitTest");
+            
+            // ensure that local folder does not exist
+            if (Directory.Exists(localPath))
+                Directory.Delete(localPath, true);
+
+
+            Repository.Clone(cloneUrl, localPath);
         }
     }
 }
